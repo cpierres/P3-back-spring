93c0325c6c519c667ea3bc7d63c809cd0eafe90d
cpminipc <cpierres@hotmail.com>
2025-01-03 22:09:06 +0100

    Initialisation du projet 3 backend pour OpenClassroom

---
+++ src/main/java/com/cpierres/p3backspring/P3BackSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cpierres.p3backspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3BackSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P3BackSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cpierres/p3backspring/P3BackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpierres.p3backspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3BackSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
