c8a18fc379601c1b902a6d9dbef2dd45f2d39c9f
cpminipc <cpierres@hotmail.com>
2025-01-12 14:02:42 +0100

    Gestion d'exceptions pour éviter erreurs 500 en cas d'id inexistant pour GET rentals/{id} et GET user/{id}

---
+++ src/main/java/com/cpierres/p3backspring/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.cpierres.p3backspring.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        // Retourne une réponse 404 avec le message de l'exception.
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(RentalNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(RentalNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
---
+++ src/main/java/com/cpierres/p3backspring/exception/RentalNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cpierres.p3backspring.exception;
+
+public class RentalNotFoundException  extends RuntimeException {
+    public RentalNotFoundException(String message) {
+        super(message);
+    }
+}
