c83b62afe070bfd50d76cb8892c413f4d3423a03
cpminipc <cpierres@hotmail.com>
2025-01-17 21:06:21 +0100

    Configuration OpenApi : permettre d'indiquer le token pour les api protégées

--- src/main/java/com/cpierres/p3backspring/configuration/RentalOpenApi.java
+++ src/main/java/com/cpierres/p3backspring/configuration/RentalOpenApi.java
@@ -1,9 +1,13 @@
 package com.cpierres.p3backspring.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +16,9 @@ public class RentalOpenApi {
 
     @Bean
     public OpenAPI myOpenApi() {
+        // Nom du schéma de sécurité (utilisé dans les requêtes)
+        String securitySchemeName = "Bearer Authentication";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Rental API")
@@ -24,6 +31,15 @@ public class RentalOpenApi {
                                 .name("Christophe Pierrès")
                                 .email("cpierres[at]hotmail.com")
                         )
-                );
+                ).addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName)) // Applique le schéma à toutes les requêtes
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT"))); // Indique que le token est de type JWT
+
     }
 }
